da389837eb341e202edefe6f3fd111bebb4ffd64
Chase Conrad <cconrad@the-alliance.org>
2022-09-27 15:56:41 -0500

    button

--- src/lib/components/content/uploadrow.svelte
+++ src/lib/components/content/uploadrow.svelte
@@ -30,13 +30,6 @@
 
 <style>
   .dropzone {
-    background-color: #4caf50; /* Green */
     border: 2px dashed black;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
   }
 </style>
